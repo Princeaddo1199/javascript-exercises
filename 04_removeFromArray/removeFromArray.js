07e9fd71d2630029761966cb1da1da13b2e43489
Prince Addo <princeaddo1199@gmail.com>
2024-03-08 18:29:06 +0000

    removed array modification commited

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -7,7 +7,7 @@ array.forEach((item) =>{
 })
     return newArray;
 };
-removeFromArray([1, 2, 3, 4], 2);
+console.log(removeFromArray([1, 2, 3, 4], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
