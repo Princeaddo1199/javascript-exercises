a50b886a6a53d44dadea82289e4e7f6e8b26911e
Prince Addo <princeaddo1199@gmail.com>
2024-03-08 18:27:31 +0000

    Used some methods and loops to do some calculations using  functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
+add(12, 8)
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
+subtract(18, 8)
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((totalArray, currentArray) => totalArray + currentArray, 0)
 };
+sum([2, 5, 3, 6, 9, 12])
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((productArray, currentArray) => productArray * currentArray)
 
 };
+multiply([2, 5, 3, 6, 9, 12])
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
+power(10, 3)
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+    let product = 1;
+    for (let i = num; i > 0; i--){
+        product *= i;
+    }
+    return product
 };
+factorial(20);
 
 // Do not edit below this line
 module.exports = {
