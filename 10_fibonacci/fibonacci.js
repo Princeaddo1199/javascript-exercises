5cd5c2417defef808652af855014e0c14522bb80
Prince Addo <princeaddo1199@gmail.com>
2024-03-08 20:17:26 +0000

    Fibonacci i dont really understand it but i will study it again

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count
+    if (typeof countArg !== 'number'){
+        count = parseInt(countArg)
+    }else{
+        count = countArg
+    }
+    if (count < 0) return "OOPS"
+    if (count == 0) return 0;
 
+    let n0 = 1;
+    let n1 = 0;
+
+    for (let i =2; i <= count; i++){
+        let current = n0 + n1;
+        n1 = n0;
+        n0 = current
+    }
+return n0
 };
+fibonacci(5)
 
 // Do not edit below this line
 module.exports = fibonacci;
