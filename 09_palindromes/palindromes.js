197c4443767d0eb98b9a3a4a555e3ab3b1e7016b
Prince Addo <princeaddo1199@gmail.com>
2024-03-08 18:42:13 +0000

    Check truth statements

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+const cleanStr = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+return cleanStr === cleanStr.split('').reverse().join('');
 };
+palindromes("radar");
+
 
 // Do not edit below this line
 module.exports = palindromes;
