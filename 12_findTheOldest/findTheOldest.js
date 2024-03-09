6f5ca8331f0831b85150a9f0730283869e255618
Prince Addo <princeaddo1199@gmail.com>
2024-03-09 06:19:13 +0000

    using array methods to find old age of people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(
+            currentPerson.yearOfBirth,
+            currentPerson.yearOfDeath
+        );
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
 };
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+findTheOldest( [
+    { name: "Micheal Opoku", yearOfBirth: 1990, yearOfDeath: 2030 },
+    { name: "Adamu Halima", yearOfBirth: 1999, yearOfDeath: 2060 }
+    ])
 
 // Do not edit below this line
 module.exports = findTheOldest;
